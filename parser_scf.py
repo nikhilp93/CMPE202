05e245a7d641e32e4fac5eafa50bcbb1020f8802
Nikhil <nprathap@cisco.com>
2020-04-13 11:14:41 -0700

    more changes

--- parser_scf.py
+++ parser_scf.py
@@ -1,7 +1,7 @@
 import codecs
 import os
 
-with open('/Users/nprathap/scfprogram/SCFFile.txt', 'r') as file1:
+with open('/Users/nprathap/scfprogram/CMPE202/SCFFile.txt', 'r') as file1:
     out1=file1.read()
     out2 = out1.replace(" ","")
     parsed_out = out2.replace("\n","")
@@ -14,7 +14,7 @@ def hex_to_dec(hex1):
 	return out
 
 #Parsing SCF header
-for i in range(1,15):
+for i in range(1,17):
 	if i==3 or i==7 or i==9:
 		type=parsed_out[:2]
 		parsed_out=parsed_out[2:]
@@ -34,13 +34,23 @@ for i in range(1,15):
 		parsed_out=parsed_out[2:]
 		print("type"+str(i)+"is: ",type)
 
-		hex_length=parsed_out[:4]
-		#print(length1)
-		dec_length= hex_to_dec(hex_length)
-		#print(dec_length1)
-		parsed_out=parsed_out[4:]
-		print("length of"+str(i)+"is: ", dec_length)
-		if i ==4 or i==6 or i==13:
+		if type=="0d":
+			print("We hit type:0d, End of SCF header\n")
+			parsed_out_body = parsed_out
+			print('Rest of the output to be parsed for SCF body is: ', parsed_out_body)
+			break
+
+		else:
+
+			hex_length=parsed_out[:4]
+			#print(length1)
+			dec_length= hex_to_dec(hex_length)
+			#print(dec_length1)
+			parsed_out=parsed_out[4:]
+			print("length of"+str(i)+"is: ", dec_length)
+		
+		if type=="04" or type=="06" or type=="0e":
+		#if i ==4 or i==6 or i==13:
 			value=parsed_out[:dec_length*2]
 			#print(value)
 			parsed_out=parsed_out[dec_length*2:]
@@ -51,8 +61,54 @@ for i in range(1,15):
 			#print(value)
 			parsed_out=parsed_out[dec_length*2:]
 			print("value of"+str(i)+"is: ", value)
+"""
+#Parsing SCF body
+for i in range(1,13):
+	if i==3 or i==7 or i==9:
+		type=parsed_out[:2]
+		parsed_out=parsed_out[2:]
+		print("type"+str(i)+"is: ",type)
 
-#type2=parsed_out[6+dec_length1:len(value1)+6+dec_length1]
-#print(type2)
+		hex_length=parsed_out[:4]
+		#print(length1)
+		dec_length= hex_to_dec(hex_length)
+		#print(dec_length1)
+		parsed_out=parsed_out[4:]
+		print("length of"+str(i)+"is: ", dec_length)
 
+	else:
+		
+		
+		type=parsed_out[:2]
+		parsed_out=parsed_out[2:]
+		print("type"+str(i)+"is: ",type)
+
+		if type=="0d":
+			print("We hit type:0d, End of SCF header\n")
+			parsed_out_body = parsed_out
+			print('Rest of the output to be parsed for SCF body is: ', parsed_out_body)
+			break
+
+		else:
+
+			hex_length=parsed_out[:4]
+			#print(length1)
+			dec_length= hex_to_dec(hex_length)
+			#print(dec_length1)
+			parsed_out=parsed_out[4:]
+			print("length of"+str(i)+"is: ", dec_length)
+		
+		if type=="04" or type=="06" or type=="0e":
+		#if i ==4 or i==6 or i==13:
+			value=parsed_out[:dec_length*2]
+			#print(value)
+			parsed_out=parsed_out[dec_length*2:]
+			print("value of"+str(i)+"is: ", value)
+			print(codecs.decode(value, "hex").decode('utf-8'))
+		else:
+			value=parsed_out[:dec_length*2]
+			#print(value)
+			parsed_out=parsed_out[dec_length*2:]
+			print("value of"+str(i)+"is: ", value)
+"""
 
