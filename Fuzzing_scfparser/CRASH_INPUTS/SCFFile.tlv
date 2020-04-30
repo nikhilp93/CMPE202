f56e8df8a34a8600cf45b095da607fa45f22911b
Rajesh <rpaturi@gmail.com>
2020-04-29 19:37:35 -0700

    adding fuzzing files

---
+++ Fuzzing_scfparser/CRASH_INPUTS/SCFFile.tlv
@@ -0,0 +1,2 @@
+ü=v*gKb®‹¿Ö“ý¬æ»Ñ˜hDÊqN$ñÎ†³¡­ÔK!&î9±¸¢ÿÃÜõ@Pu{|úŽãto'éý=.bÖ>ñËÖñêÔ÷©²2eÁÄÂå¦äÎ2‹”Ëºç²S~é
+6À?›  ƒVµ”PuÓÙ×OÇáÔÞKã7›uòÐØ>Úsªdˆƒ¯7º–öDèv0ë]æ{Qõ†µl’ ‹¾çÚ®ÜÞW@ÿ?•dô ‘õ(²VA·'6[‰ÈMÝWa(R¢0*0÷²`“GHÐyùßt4}‹OdN`PÅ—î¶Í@žhbH×“%:e«÷í¦:.ÊYžÏ‡-	°¶)Œ
---
+++ Fuzzing_scfparser/Fuzz-SCFParser.py
@@ -0,0 +1,51 @@
+import fuzzbang
+import subprocess
+import re
+import datetime
+import shutil
+import time
+from fuzzbang.alphanumericfuzzer import  AlphaNumericFuzzer
+from fuzzbang.binaryfuzzer import  BinaryFuzzer
+
+N = 1 # number of test cases
+
+# bounds on length of strings
+MIN_LEN = 0
+MAX_LEN = 2
+
+f1 = AlphaNumericFuzzer(MIN_LEN, MAX_LEN) # fuzzer object
+f = BinaryFuzzer(MIN_LEN, MAX_LEN)
+
+
+    # generate test cases
+for i in range(N):
+        time.sleep(2)
+        with open("c:\project\SCFProgram\SCFFile.tlv","wb") as fp:
+            for j in range(N):
+                data = f.generate() # generate string
+                fp.write(data)
+                #fp.write(out.stdout.decode('UTF-8'))
+                print("(" + str(len(data)) + ")") # print length of string
+                print(data) # print string itself
+
+        out=subprocess.run(["SCFParser.exe"],stdout=subprocess.PIPE)
+        print(out)
+        print(out.stdout.decode('UTF-8'),end='\n')
+        if re.search("Buffer Overflow",out.stdout.decode('UTF-8')):
+
+            ts=time.time()
+            st = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
+            crash_summary_file_name="crash_summary"+str(st)+".txt"
+            print("The sample caused buffer overflow.Crash summary in %s" %crash_summary_file_name )
+            with open(crash_summary_file_name,"w") as fp1:
+                fp1.write(out.stdout.decode('UTF-8'))
+            shutil.move(crash_summary_file_name,"CRASH_INPUTS")
+            target_name="c:\project\SCFProgram\CRASH_INPUTS\SCFFile"+str(st)+".tlv"
+            shutil.move("SCFFile.tlv",target_name)
+
+
+
+
+
+
+
